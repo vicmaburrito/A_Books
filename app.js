d45db83326caf4a743d2b73b1b068dc0d54c53cf
vicmaburrito <vmaldaraca@gmail.com>
2021-11-03 22:13:48 -0600

    Add display function and fix linter errors

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// book class and constructor
+// book class - represents a book
 class Book {
   constructor(id, title, author) {
     this.id = id;
@@ -6,46 +6,68 @@ class Book {
     this.author = author;
   }
 
-// get books from localstorage
-static getBooks = () => {
-  let books;
+  // get books from localstorage
+  static getBooks = () => {
+    let books;
 
-  if (localStorage.getItem('books') === null) {
-    books = [];
-  } else {
-    books = JSON.parse(localStorage.getItem('books'));
-  }
-
-  return books;
-};
-
-    // display books
-
-    // delete book from the browser view
-    static deleteBook = (el) => {
-      if (el.classList.contains('delete'));
-      el.parentElement.remove();
-    };
-
-    // function to add a new book to the collection, with title and author.
-    static addBook = (book) => {
-      const books = Book.getBooks();
-      books.push(book);
-      localStorage.setItem('books', JSON.stringify(books));
-    };
-
-    // delete book from local storage
-    static removeBook = (id) => {
-      const books = Book.getBooks();
-
-  books.forEach((book, index) => {
-    if (book.id === id) {
-      books.splice(index, 1);
+    if (localStorage.getItem('books') === null) {
+      books = [];
+    } else {
+      books = JSON.parse(localStorage.getItem('books'));
     }
-  });
 
-  localStorage.setItem('books', JSON.stringify(books));
-};
+    return books;
+  };
+
+  // display books
+  static displayBooks = () => {
+    const books = Book.getBooks();
+    books.forEach((book) => Book.addBookToList(book));
+  };
+
+  // add submitted book to browser view
+  static addBookToList = (book) => {
+    const list = document.getElementById('bookList');
+
+    const row = document.createElement('div');
+    row.innerHTML = `
+      <div>
+      <div>${book.title}</div>
+      <div>${book.author}</div>
+      <div hidden>${book.id}</div>
+      <button class="delete">Remove</button>
+      <hr>
+      </div>
+    `;
+
+    list.appendChild(row);
+  };
+
+  // delete book from the browser view
+  static deleteBook = (el) => {
+    if (el.classList.contains('delete'));
+    el.parentElement.remove();
+  };
+
+  // function to add a new book to the collection, with title and author.
+  static addBook = (book) => {
+    const books = Book.getBooks();
+    books.push(book);
+    localStorage.setItem('books', JSON.stringify(books));
+  };
+
+  // delete book from local storage
+  static removeBook = (id) => {
+    const books = Book.getBooks();
+
+    books.forEach((book, index) => {
+      if (book.id === id) {
+        books.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('books', JSON.stringify(books));
+  };
 }
 
 // display books list on window load
